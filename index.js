44e2b6b3da09cb01022ed6bcaee8cc83d53c593c
Samael Szep <szepsamael@gmail.com>
2023-06-15 18:11:35 +0200

    final commit

--- index.js
+++ index.js
@@ -7,12 +7,6 @@ import {
 } from "./components/nav-button/nav-button.js";
 
 const cardContainer = document.querySelector('[data-js="card-container"]');
-const searchBarContainer = document.querySelector(
-  '[data-js="search-bar-container"]'
-);
-const navigation = document.querySelector('[data-js="navigation"]');
-// const prevButton = document.querySelector('[data-js="button-prev"]');
-// const nextButton = document.querySelector('[data-js="button-next"]');
 const pagination = document.querySelector('[data-js="pagination"]');
 
 let maxPage = 42;
@@ -46,19 +40,3 @@ fetchCharacter(page, searchQuery);
 handleSearchSubmit();
 handleNextButton();
 handlePrevButton();
-
-// nextButton.addEventListener("click", () => {
-//   if (page < maxPage) {
-//     page = page + 1;
-//     fetchCharacter(page, searchQuery);
-//     updatePagination(page, maxPage, pagination);
-//   }
-// });
-
-// prevButton.addEventListener("click", () => {
-//   if (page > 1) {
-//     page = page - 1;
-//     fetchCharacter(page, searchQuery);
-//     updatePagination(page, maxPage, pagination);
-//   }
-// });
